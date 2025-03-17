cc651c033f7fd47e4ae6c1c50925590be305808c
Jayavel <jayavel1520@gmail.com>
2025-03-17 15:25:56 +0530

    add Background component with animated background effect; update App to include Background

--- urlShortner/src/App.jsx
+++ urlShortner/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SignUp from './SignUp'
 import Login from './Login'
+import Background from './Background'
 const App = () => {
   return (
     <div className='main'>
         <Login/>
+        <Background/>
     </div>
   )
 }
---
+++ urlShortner/src/Background.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Background = () => {
+    return (
+        <div>
+            <ul class="background">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Background
