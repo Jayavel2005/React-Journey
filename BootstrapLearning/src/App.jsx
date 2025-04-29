62e3e17b6983c83ed32e38a9b16ca7d32f86b078
Jayavel <jayavel1520@gmail.com>
2025-04-29 15:49:40 +0530

    Bootstrap Typography and Image Learning

--- BootstrapLearning/src/App.jsx
+++ BootstrapLearning/src/App.jsx
@@ -1,14 +1,72 @@
 import React from 'react';
-import Text from './Text';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap"; // Importing Bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import Image from 'react-bootstrap/Image'; // Importing Image component from Bootstrap
+import image1 from '../src/assets/image1.jpeg'; // Importing an image from assets
 
 const App = () => {
   return (
-    <div>
-        <p className='display-5'>Hello Bootstrap</p>
-        <Text/>
-    </div>
+    <Container className='border border-1'> {/* Main container with a border */}
+
+      {/* Headings */}
+      <h1>Hello</h1> {/* Standard heading */}
+      <h3>Hello</h3> {/* Smaller heading */}
+      <p>Hello</p> {/* Paragraph text */}
+
+      {/* Headings styled as paragraphs */}
+      <p className='h1'>Hello</p> {/* Paragraph styled as h1 */}
+      <p className='h2'>Hello</p> {/* Paragraph styled as h2 */}
+
+      {/* Display Headings */}
+      <h1 className="display-1">Hello</h1> {/* Largest display heading */}
+      <h1 className="display-3">Hello</h1> {/* Medium display heading */}
+      <h1 className="display-5">Hello</h1> {/* Smallest display heading */}
+
+      {/* Lead and muted text */}
+      <p className='lead text-muted'>Lorem, <s>ipsum</s> dolor sit amet consectetur adipisicing elit. Ad, tempora.</p> {/* Lead text with muted color and strikethrough */}
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius, delectus!</p> {/* Regular paragraph */}
+
+      {/* Text alignment */}
+      <p className='display-1 text-center'>Hello World!</p> {/* Center-aligned text */}
+      <p className='display-1 text-start'>Hello World!</p> {/* Left-aligned text */}
+      <p className='display-1 text-end'>Hello World!</p> {/* Right-aligned text */}
+
+      {/* Text transform */}
+      <p className='display-1 text-uppercase'>Hello React!</p> {/* Uppercase text */}
+      <p className='display-1 text-lowercase'>Hello React!</p> {/* Lowercase text */}
+      <p className='display-1 text-capitalize'>Hello React!</p> {/* Capitalized text */}
+
+      {/* Font size */}
+      <p className='fs-1'>Font Size 1</p> {/* Largest font size */}
+      <p className='fs-2'>Font Size 2</p> {/* Second largest font size */}
+      <p className='fs-3'>Font Size 3</p> {/* Medium font size */}
+      <p className='fs-4'>Font Size 4</p> {/* Smaller font size */}
+      <p className='fs-5'>Font Size 5</p> {/* Smaller font size */}
+      <p className='fs-6'>Font Size 6</p> {/* Smallest font size */}
+
+      {/* Font weight and styles */}
+      <p className='display-1 fw-bold'>Hello Bootstrap</p> {/* Bold font */}
+      <p className='display-1 fw-bolder'>Hello Bootstrap</p> {/* Bolder font */}
+      <p className='display-1 fw-light'>Hello Bootstrap</p> {/* Light font */}
+      <p className='display-1 fw-medium'>Hello Bootstrap</p> {/* Medium font */}
+
+      {/* Text decoration */}
+      <p className="text-decoration-underline">This text has a line underneath it.</p> {/* Underlined text */}
+      <p className="text-decoration-line-through">This text has a line going through it.</p> {/* Strikethrough text */}
+      {/* <a href="#" class="text-decoration-none">This link has its text decoration removed</a> */} {/* Link with no text decoration (commented out) */}
+
+      {/* Image */}
+      <Image 
+        src={image1} 
+        fluid={true} 
+        rounded={true} 
+        thumbnail={true} 
+        className='w-100 object-fit-cover' 
+        alt='image' 
+      /> {/* Responsive image with rounded corners, thumbnail styling, and full width */}
+
+    </Container>
   )
 }
 
-export default App
+export default App;
