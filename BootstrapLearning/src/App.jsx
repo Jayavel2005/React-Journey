483855a182b114cd8423d02e18a7cc22a5e1f0b6
Jayavel <jayavel1520@gmail.com>
2025-04-29 13:38:57 +0530

    learning react bootstrap

---
+++ BootstrapLearning/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from './Text';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+        <p className='display-5'>Hello Bootstrap</p>
+        <Text/>
+    </div>
+  )
+}
+
+export default App
---
+++ BootstrapLearning/src/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div>
+        <p className='display-2 text-dark'>Hello Jayavel</p>
+    </div>
+  )
+}
+
+export default Text
